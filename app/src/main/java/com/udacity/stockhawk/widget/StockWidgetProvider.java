7f3ebfddfddce6ea6629429262ed3dd2a4933aae
Ramon Lence <rlence86@gmail.com>
2017-05-22 02:06:48 +0200

    added click listener to widget

--- app/src/main/java/com/udacity/stockhawk/widget/StockWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/widget/StockWidgetProvider.java
@@ -1,13 +1,17 @@
 package com.udacity.stockhawk.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.app.TaskStackBuilder;
 import android.widget.RemoteViews;
 
 import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.ui.DetailedViewActivity;
+import com.udacity.stockhawk.ui.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -19,6 +23,9 @@ public class StockWidgetProvider extends AppWidgetProvider {
         for (int widgetId : appWidgetIds){
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_stock_list);
             setRemoteAdapter(context, remoteViews);
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            remoteViews.setOnClickPendingIntent(R.id.widget, pendingIntent);
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
     }
